5c9f4813dba7d9895a45000909e21f011a72d655
anuj-af <anujkhandelwal.0404@gmail.com>
2024-06-02 10:03:58 +0530

    changed image-upload in edit page to accept multiple images thorugh upload files

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,12 +1,12 @@
 const Campground = require('../models/campground');
 
-module.exports.index = async (req,res) => {
+module.exports.index = async (req, res) => {
     const campgrounds = await Campground.find({}).populate('images');
-    if(campgrounds.length === 0) throw new ExpressError('Sorry No Campgrounds Available');
-    res.render('campgrounds/index',{campgrounds});
+    if (campgrounds.length === 0) throw new ExpressError('Sorry No Campgrounds Available');
+    res.render('campgrounds/index', { campgrounds });
 }
 
-module.exports.createCampground = async (req,res) => {
+module.exports.createCampground = async (req, res) => {
     // if(!req.body) throw new ExpressError('Invalid Campground Data',400);
     // This is schema has nothing to do with mongoose schema we even define this before defining mongoose schema
     // const campgroundSchema = Joi.object({
@@ -24,9 +24,9 @@ module.exports.createCampground = async (req,res) => {
     //     const msg = error.details.map(elm => elm.message).join(',');
     //     throw new ExpressError(msg,400)
     // }
-    
+
     const campground = new Campground(req.body);
-    campground.images = req.files.map(f => ({url: f.path, filename: f.filename})); //Implicit return
+    campground.images = req.files.map(f => ({ url: f.path, filename: f.filename })); //Implicit return
     campground.author = req.user._id;
     await campground.save();
     console.log(campground);
@@ -34,38 +34,41 @@ module.exports.createCampground = async (req,res) => {
     res.redirect(`/campgrounds/${campground._id}`);
 }
 
-module.exports.renderNewForm = (req,res) => {
+module.exports.renderNewForm = (req, res) => {
     res.render('campgrounds/new');
 }
 
-module.exports.showCampground = async (req,res,next) => {
-    const {id} = req.params;
-    const campground = await Campground.findById(id).populate({path: 'reviews', populate:{path: 'author'}}).populate('author').populate('images');//Nested populate
+module.exports.showCampground = async (req, res, next) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id).populate({ path: 'reviews', populate: { path: 'author' } }).populate('author').populate('images');//Nested populate
     console.log(campground);
     // if(!campground) throw new ExpressError('Campground Not Found',404);
-    if(!campground){
-        req.flash('error','Cannot find that Found');
+    if (!campground) {
+        req.flash('error', 'Cannot find that Found');
         return res.redirect('/campgrounds');
-    } 
-    res.render('campgrounds/show',{campground});
+    }
+    res.render('campgrounds/show', { campground });
 }
 
-module.exports.updateCampground = async (req,res,next) => {
-    const {id} = req.params;
-    const campground = await Campground.findByIdAndUpdate(id,req.body,{runValidators: true, new: true});
+module.exports.updateCampground = async (req, res, next) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
+    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename })); //Implicit return
+    campground.images.push(...imgs); //Spreading array as we want to push elements into an array not array into an array
+    await campground.save();
     req.flash('success', 'Successfully updated the Campground');
     res.redirect(`/campgrounds/${campground._id}`);
 }
 
-module.exports.deleteCampground = async (req,res) => {
-    const {id} = req.params;
+module.exports.deleteCampground = async (req, res) => {
+    const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     req.flash('success', 'Successfully deleted the Campground');
     res.redirect('/campgrounds');
 }
 
-module.exports.renderEditForm = async (req,res) => {
-    const {id} = req.params;
+module.exports.renderEditForm = async (req, res) => {
+    const { id } = req.params;
     const campground = await Campground.findById(id);
-    res.render('campgrounds/edit',{campground});
+    res.render('campgrounds/edit', { campground });
 }
